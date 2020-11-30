ba49bb3ae1c49975171611394ddac90959276a3e
onyschukoleh <onyschukoleh@gmail.com>
2020-12-01 00:17:55 +0200

    Add some correct

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 // ///////////////////////////////////////
 // //Задание 1 - Профиль социальной сети
 // //////////////////////////////////////
-import user from './user.json';
-import Profile from './components/Profile';
-export default function App() {
-  return (
-    <div>
-      <Profile
-        name={user.name}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-        
-      />
-    </div>
-  );
-}
+// import user from './user.json';
+// import Profile from './components/Profile';
+// export default function App() {
+//   return (
+//     <div>
+//       <Profile
+//         name={user.name}
+//         tag={user.tag}
+//         location={user.location}
+//         avatar={user.avatar}
+//         stats={user.stats}
+
+//       />
+//     </div>
+//   );
+// }
 
 // ///////////////////////////////////////
 // //Задание 2 - Секция статистики
@@ -44,15 +44,15 @@ export default function App() {
 //   );
 // }
 
-// ///////////////////////////////////////
-// //Задание 4 - История транзакций
-// //////////////////////////////////////
-// import transactions from './transactions.json';
-// import TransactionHistory from './components/TransactionHistory';
-// export default function App() {
-//   return (
-//     <div>
-//       <TransactionHistory transactions={transactions} />
-//     </div>
-//   );
-// }
+///////////////////////////////////////
+//Задание 4 - История транзакций
+//////////////////////////////////////
+import transactions from './transactions.json';
+import TransactionHistory from './components/TransactionHistory';
+export default function App() {
+  return (
+    <div>
+      <TransactionHistory transactions={transactions} />
+    </div>
+  );
+}
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,28 +1,35 @@
-export default function Profile({ name,location,tag,avatar,followers,views,likes}) {
+export default function Profile({
+  name,
+  location,
+  tag,
+  avatar,
+  followers,
+  views,
+  likes,
+}) {
   return (
-    <div class="profile">
-     <div class="description">
-       <img src={avatar} alt="Аватар пользователя" class="avatar" />
-       <p class="name">{name}</p>
-       <p class="tag">@{tag}</p>
-       <p class="location">{location}</p>
-     </div>
+    <div className="profile">
+      <div className="description">
+        <img src={avatar} alt="Аватар пользователя" className="avatar" />
+        <p className="name">{name}</p>
+        <p className="tag">@{tag}</p>
+        <p className="location">{location}</p>
+      </div>
 
-     <ul class="stats">
-       <li>
-         <span class="label">Followers</span>
-         <span class="quantity">{followers}</span>
-       </li>
-       <li>
-         <span class="label">Views</span>
-         <span class="quantity">{views}</span>
-       </li>
-       <li>
-         <span class="label">Likes</span>
-         <span class="quantity">{likes}</span>
-       </li>
-     </ul>
-   </div>
+      <ul className="stats">
+        <li>
+          <span className="label">Followers</span>
+          <span className="quantity">{followers}</span>
+        </li>
+        <li>
+          <span className="label">Views</span>
+          <span className="quantity">{views}</span>
+        </li>
+        <li>
+          <span className="label">Likes</span>
+          <span className="quantity">{likes}</span>
+        </li>
+      </ul>
+    </div>
   );
 }
-
